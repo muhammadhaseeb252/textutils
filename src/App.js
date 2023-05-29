db3a3977d8dc3e6076e1ccb4b8c570c7d963a9f4
muhammadhaseeb252 <engineer.haseeb252@gmail.com>
2023-05-29 21:07:24 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+
+import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+
+
+function App() {
+
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type)=>{
+    setAlert(
+      {
+        msg: message,
+        type: type
+      }
+    )
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+
+  const [mode, setMode] = useState('light');
+  const toggleMode=()=>{
+    if (mode==='light') {
+      setMode('dark');
+      //change background color
+      document.body.style.backgroundColor = '#042743';
+      showAlert('Dark mode has been enabled', 'success');
+
+      //change title
+      // document.title = 'TextUtils - Dark Mode';
+      // setInterval(() => {
+      //   document.title = 'TextUtils is Amazing';
+      // }, 1500);
+      // setInterval(() => {
+      //   document.title = 'Try It';
+      // }, 3000);
+      
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert('Light mode has been enabled', 'success');
+      // document.title = 'TextUtils - Light Mode';
+    }
+  }
+
+  return (
+    <>
+    <Router>
+
+      <Navbar title='Textutils' hometext='Home' abouttext='About Us' mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
+      
+      <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/">
+            <TextForm heading='Enter text to analyze:' mode={mode} showAlert={showAlert}/>
+          </Route>
+      </Switch>
+
+    </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,79 @@
+import React, {useState} from 'react'
+
+
+export default function TextForm(props) {
+
+    // hooks
+    const [text, setText] = useState('Enter text here...');
+
+    const change = (event)=>{
+        // console.log('on change');
+        setText(event.target.value);
+    }
+
+    const uclick = ()=>{
+        // console.log('button clicked');
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert('Converted to upercase','success');
+    }
+
+    const lclick = ()=>{
+        // console.log('button clicked');
+        let newText = text.toLowerCase();
+        setText(newText);
+        props.showAlert('Converted to lowercase', 'success');
+    }
+
+    const cclick = ()=>{
+        // console.log('button clicked');
+        let newText ='';
+        setText(newText);
+    }
+
+    // copt to clipboard
+    const clclick = ()=>{
+        navigator.clipboard.writeText(text);
+        props.showAlert('Copied to clipboard', 'success');
+    }
+
+    // remove extraspaces
+    const rclick = ()=>{
+        var newText = text.split(/[ ]+/);
+        setText(newText.join(' '));
+    }
+
+    
+
+    return (
+        <>
+        <div className='container my-2' style={
+            {color: props.mode==='light'?'black':'white'}   
+            }>
+            <div className="mb-3">
+                <h3>{props.heading}</h3>
+                {/* <label htmlFor="textbox" class="form-label">Example textarea</label> */}
+                <textarea className="form-control" onChange={change} value={text} id="mytext" rows="8" style={
+                 {backgroundColor: props.mode==='light'?'white':'gray', color: props.mode==='light'?'black':'white'}   
+                }></textarea>
+            </div>
+            <button className="btn btn-primary" onClick={uclick}>Convert to upercase</button>
+            <button className='btn btn-success mx-1' onClick={lclick}>Convert to lowercase</button>
+            <button className='btn btn-success' onClick={cclick}>Clear text</button>
+            <button className='btn btn-warning mx-1' onClick={clclick}>Copy to clip</button>
+            <button className='btn btn-danger' onClick={rclick}>Remove Extra Spaces</button>
+        </div>
+        <div className="container" style={
+            {color: props.mode==='light'?'black':'white'}   
+            }>
+            <h3>Your text summary</h3>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} minutes to read.</p>
+            <h4>Preview</h4>
+            <p>{text.length>0?text: 'Write something in the above box to preview...'}</p>
+        </div>
+        </>
+        
+
+    )
+}
